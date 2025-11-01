3dd2529b6846bf269cff23300e3d8fcbd4f257fa
CrisPlays80 <cristian.steven05@gmail.com>
2025-11-01 17:00:37 -0500

    Add: Añado configuraciones para la base de datos y sus modelos, como: -users -products -orders -orderitems

---
+++ app/db/models.py
@@ -0,0 +1,53 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime, func
+from sqlalchemy.orm import relationship
+from app.db.base import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: int = Column(Integer, primary_key=True, index=True)
+    email: str = Column(String, unique=True, nullable=False)
+    hashed_password: str = Column(String, nullable=False)
+    full_name: str = Column(String, nullable=True)
+    role: str = Column(String, default="customer", nullable=False)
+    created_at: DateTime = Column(DateTime(timezone=True), server_default=func.now())
+
+    orders = relationship("Order", back_populates="user")
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id: int = Column(Integer, primary_key=True, index=True)
+    sku: int = Column(Integer, unique=True, nullable=False)
+    name: str = Column(String, index=True, nullable=False)
+    description: str = Column(String, nullable=True)
+    price: float = Column(Float, nullable=False)
+
+    orders = relationship("Order", back_populates="product")
+
+
+class Order(Base):
+    __tablename__ = "orders"
+    
+    id: int = Column(Integer, primary_key=True, index=True)
+    total_amount: float = Column(Float, nullable=False, default=0.0)
+    created_at: DateTime = Column(DateTime(timezone=True), server_default=func.now())
+    
+    user_id: int = Column(Integer, ForeignKey("users.id"), nullable=False)
+    user = relationship("User", back_populates="orders", foreign_keys=[user_id])
+    
+    items = relationship("OrderItem", back_populates="order")
+
+class OrderItem(Base):
+    __tablename__ = "order_items"
+
+    id: int = Column(Integer, primary_key=True, index=True)
+    
+    order_id: int = Column(Integer, ForeignKey("orders.id"), nullable=False)
+    
+    product_id: int = Column(Integer, ForeignKey("products.id"), nullable=False)
+
+    quantity: int = Column(Integer, nullable=False)
+    
+    order = relationship("Order", back_populates="items")
+    product = relationship("Product", back_populates="order_items")
