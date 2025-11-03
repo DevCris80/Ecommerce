8684033fcac956c32b476dcee76a56f7643da7c7
CrisPlays80 <cristian.steven05@gmail.com>
2025-11-03 18:31:19 -0500

    Add: Schema users

---
+++ app/schemas/users.py
@@ -0,0 +1,19 @@
+from typing import Optional
+from pydantic import BaseModel, EmailStr
+
+class UserBase(BaseModel):
+    email: EmailStr
+    full_name: Optional[str] = None
+
+
+class UserCreate(UserBase):
+    password: str
+    role: str = "customer"
+    
+
+class UserRead(UserBase):
+    id: int
+    created_at: Optional[str] = None
+
+    class Config:
+        orm_mode = True
