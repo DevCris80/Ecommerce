af28e87a75e943240ab6af33e2cfbf53b07c3428
CrisPlays80 <cristian.steven05@gmail.com>
2025-11-05 17:27:37 -0500

    Add: Include products router in main application

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.api.v1.users import router as users_router
+from app.api.v1.products import router as products_router
 
 app = FastAPI()
 
 app.include_router(users_router, prefix="/api/v1/users", tags=["users"])
+app.include_router(products_router, prefix="/api/v1/products", tags=["products"])
