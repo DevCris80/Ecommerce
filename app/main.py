d76101c6d8bcbdda0d519a9cf85863966626003c
CrisPlays80 <cristian.steven05@gmail.com>
2025-11-05 17:54:52 -0500

    Add: Import Base and create database tables in main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from app.api.v1.users import router as users_router
 from app.api.v1.products import router as products_router
+from app.db.session import engine
+from app.db.base import Base
+from app.db import models 
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
