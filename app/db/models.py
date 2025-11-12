b1c00eba2282278b8c4e6c0d76dbcfdce9c36f69
CrisPlays80 <cristian.steven05@gmail.com>
2025-11-11 21:12:35 -0500

    Fix: Error que daba el datetime, ya que no servia, ademas ahora se cambia el modo de como se hace la anotacion

--- app/db/models.py
+++ app/db/models.py
@@ -1,40 +1,41 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime, func
+from sqlalchemy import ForeignKey, func
 from sqlalchemy.orm import relationship
+from typing import Mapped, mapped_column
+from datetime import datetime
+
 from app.db.base import Base
 
 class User(Base):
     __tablename__ = "users"
 
-    id: int = Column(Integer, primary_key=True, index=True)
-    email: str = Column(String, unique=True, nullable=False)
-    hashed_password: str = Column(String, nullable=False)
-    full_name: str = Column(String, nullable=True)
-    role: str = Column(String, default="customer", nullable=False)
-    created_at: DateTime = Column(DateTime(timezone=True), server_default=func.now())
-
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
+    full_name: Mapped[str] = mapped_column(nullable=True)
+    role: Mapped[str] = mapped_column(default="customer", nullable=False)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
     orders = relationship("Order", back_populates="user")
 
 class Product(Base):
     __tablename__ = "products"
 
-    id: int = Column(Integer, primary_key=True, index=True)
-    sku: str = Column(String, unique=True, nullable=False, index=True)
-    name: str = Column(String, index=True, nullable=False)
-    description: str = Column(String, nullable=True)
-    price: float = Column(Float, nullable=False)
-    created_at: DateTime = Column(DateTime(timezone=True), server_default=func.now())
-
+    id: Mapped[int] =  mapped_column(primary_key=True, index=True)
+    sku: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
+    name: Mapped[str] = mapped_column(index=True, nullable=False)
+    description: Mapped[str] = mapped_column(nullable=True)
+    price: Mapped[float] = mapped_column(nullable=False)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
     order_items = relationship("OrderItem", back_populates="product")
 
 
 class Order(Base):
     __tablename__ = "orders"
     
-    id: int = Column(Integer, primary_key=True, index=True)
-    total_amount: float = Column(Float, nullable=False, default=0.0)
-    created_at: DateTime = Column(DateTime(timezone=True), server_default=func.now())
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    total_amount: Mapped[float] = mapped_column(nullable=False, default=0.0)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
     
-    user_id: int = Column(Integer, ForeignKey("users.id"), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     user = relationship("User", back_populates="orders", foreign_keys=[user_id])
     
     items = relationship("OrderItem", back_populates="order")
@@ -42,13 +43,11 @@ class Order(Base):
 class OrderItem(Base):
     __tablename__ = "order_items"
 
-    id: int = Column(Integer, primary_key=True, index=True)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    quantity: Mapped[int] = mapped_column(nullable=False)
     
-    order_id: int = Column(Integer, ForeignKey("orders.id"), nullable=False)
-    
-    product_id: int = Column(Integer, ForeignKey("products.id"), nullable=False)
-
-    quantity: int = Column(Integer, nullable=False)
+    order_id: Mapped[int] = mapped_column(ForeignKey("orders.id"), nullable=False)
+    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"), nullable=False)
     
     order = relationship("Order", back_populates="items")
     product = relationship("Product", back_populates="order_items")
