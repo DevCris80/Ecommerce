8c69f465e3175dd5370ee1d86d79b5a756d6505f
CrisPlays80 <cristian.steven05@gmail.com>
2025-12-24 14:15:21 -0500

    Add: Logic for create_access_token

--- app/services/auth.py
+++ app/services/auth.py
@@ -1,14 +1,26 @@
 import jwt
 from datetime import datetime, timedelta, timezone
+from typing import Any
 
 from app.core.config import settings
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 SECRET_KEY = settings.SECRET_KEY
 
-
-def create_access_token(data: dict) -> str:
-    to_encode: dict = data.copy()
+def create_access_token(subject: str | Any, expires_delta: timedelta | None = None) -> str:
+    to_encode = {"sub": str(subject)}
     now = datetime.now(timezone.utc)
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm="HS256")
+    
+    if expires_delta:
+        expire = now + expires_delta
+    else:
+        expire = now + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    to_encode.update({"exp": expire, "iat": now})
+    
+    encoded_jwt = jwt.encode(
+        to_encode, 
+        settings.SECRET_KEY, 
+        algorithm=settings.ALGORITHM
+    )
     return encoded_jwt
