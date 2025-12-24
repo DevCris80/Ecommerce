bd03f46e3c83c4b92370dbbc88004b8512e54fc4
CrisPlays80 <cristian.steven05@gmail.com>
2025-12-24 14:17:10 -0500

    Add: New router

---
+++ app/api/v1/auth.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from typing import Annotated
+
+from app.api.v1.deps import get_user_service
+from app.services.user_logic import UserService
+from app.services.auth import create_access_token
+from app.schemas.auth import TokenResponse
+
+router = APIRouter()
+
+@router.post("/login", response_model=TokenResponse)
+async def login(
+    token_request: Annotated[OAuth2PasswordRequestForm, Depends()],
+    user_service: UserService = Depends(get_user_service)
+):
+    user = user_service.authenticate_user(token_request.username, token_request.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = create_access_token(user.user_id)
+    return TokenResponse(access_token=access_token, token_type="bearer")
--- app/main.py
+++ app/main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 from app.api.v1.users import router as users_router
 from app.api.v1.products import router as products_router
 from app.api.v1.orders import router as orders_router
+from app.api.v1.auth import router as login_router
 
 app = FastAPI()
 
 app.include_router(users_router, prefix="/api/v1/users", tags=["users"])
 app.include_router(products_router, prefix="/api/v1/products", tags=["products"])
 app.include_router(orders_router, prefix="/api/v1/orders", tags=["orders"])
+app.include_router(login_router, prefix="/api/v1/auth", tags=["auth"])
 
 @app.get("/")
 async def root():
