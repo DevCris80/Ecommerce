1d3135c312dc12f8903303afd24f1288c588d94b
CrisPlays80 <cristian.steven05@gmail.com>
2026-01-06 17:51:08 -0500

    Add: Model to put messages generic

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -3,3 +3,7 @@ from pydantic import BaseModel
 class TokenResponse(BaseModel):
     access_token: str
     token_type: str = "bearer"
+
+# Generic Messages 
+class Message(BaseModel):
+    message: str
