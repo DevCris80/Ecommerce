aafa3c93f1516a7e3dbd456abd20887840cc3603
CrisPlays80 <cristian.steven05@gmail.com>
2026-01-06 18:37:10 -0500

    Add: Minutes of token expire minutes

--- app/core/config.py
+++ app/core/config.py
@@ -8,6 +8,7 @@ class Settings(BaseSettings):
     POSTGRES_PORT: int
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     class Config:
         env_file = ".env"
