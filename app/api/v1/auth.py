6c33b31091a11405eb087be63028898028b87d68
CrisPlays80 <cristian.steven05@gmail.com>
2026-01-06 18:36:07 -0500

    Change structure

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -14,12 +14,21 @@ async def login(
     token_request: Annotated[OAuth2PasswordRequestForm, Depends()],
     user_service: UserService = Depends(get_user_service)
 ):
-    user = user_service.authenticate_user(token_request.username, token_request.password)
+    user = await user_service.authenticate_user(
+        token_request.username, 
+        token_request.password
+    )
+
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect email or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token = create_access_token(user.user_id)
-    return TokenResponse(access_token=access_token, token_type="bearer")
+    
+    access_token = create_access_token(subject=str(user.user_id))
+
+    return TokenResponse(
+        access_token=access_token, 
+        token_type="bearer"
+    )
